7d7c4b3caddea5851d41045f25f15d258d226a12
Aron Culotta <aronwc@gmail.com>
2013-07-28 07:35:59 -0500

    print extraction stats

--- kkff/extract.py
+++ kkff/extract.py
@@ -52,32 +52,49 @@ def looks_like_finding_header(line):
                     ').*', line.strip()[0:50].lower())
 
 
+def clean_s(s):
+    return re.sub(r'\s+', ' ', s).strip()
+
+
 def filter_sentences(sentences):
     'Filter things that dont look like sentenes'
-    pass
+    return [clean_s(s) for s in sentences if len(s.strip()) > 30]
 
 
 def summarize(lines, start, end):
+    global num_keys
     text = ' '.join(lines[start:end])
-    sentences = sent_tokenize(text)
-    print sentences
+    sentences = filter_sentences(sent_tokenize(text))
+    num_keys += len(sentences)
+    print '\n\t+++'.join(sentences)
     print '#sent=', len(sentences)
 
 
 def extract_findings(pdf):
+    global num_found
     text, err = pdf2txt(pdf)
     #print text
     lines = text.split('\n')
+    found = False
     for (i, line) in enumerate(lines):
         if (looks_like_finding_header(line)):
-            print 'HEADER:', line
+            print 'HEADER:', clean_s(line)
             summarize(lines, i + 1, i + 20)
+            found = True
+    if found:
+        num_found += 1
+
+
+num_found = 0
+num_keys = 0
 
 
 def main():
     for pdf in args.pdfs:
         print '\n\nextracting from', pdf
         extract_findings(pdf)
+    print 'found %d findings in %d/%d pdfs' % \
+        (num_keys, num_found, len(args.pdfs))
 
 if (__name__ == '__main__'):
     if args.test:
