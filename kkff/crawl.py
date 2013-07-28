be8c2baa781a944cc069d8c39688a9e2f3623735
Aron Culotta <aronwc@gmail.com>
2013-07-28 06:46:29 -0500

    first pass at crawl (#1)

---
+++ kkff/crawl.py
@@ -0,0 +1,25 @@
+'Crawl PDFs list on water.issuelab.org.'
+from bs4 import BeautifulSoup
+import re
+import requests
+
+
+soup = BeautifulSoup(requests.get('http://water.issuelab.org/home/' +
+                                  'limit/0/view/all').text)
+for div in soup.find_all('div', class_='txt'):
+    url = div.h3.a['href']
+    name = re.match(r'.*resource\/(.+)', url).group(1)
+    report_html = 'http://water.issuelab.org/' + url
+    print 'fetching', report_html
+    report_soup = BeautifulSoup(requests.get(report_html).text)
+    redirect = report_soup.h2.a['href']
+    redirect_text = requests.get(redirect).text
+    match = re.match(r'.*url=([^\"]+)\"', redirect_text)
+    if match:
+        pdf = match.group(1)
+        print '\tfetching', pdf
+        r = requests.get(pdf)
+        with open('../data/' + name + '.pdf', "wb") as outf:
+            outf.write(r.content)
+    else:
+        print 'no report found for', report_html
